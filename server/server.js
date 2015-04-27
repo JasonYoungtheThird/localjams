bb86f5e6ef4c2c850ca0c64b83022d20224851fb
Richard Young <richard.young@utexas.edu>
2015-04-26 22:54:51 -0500

    angular connection established

--- server/server.js
+++ server/server.js
@@ -1,12 +1,7 @@
 var express = require('express')
 var app = express()
 
-// app.get('/',function(req, res){
-//   res.send('Hello World')
-// })
-
 app.use(express.static('client'))
 
 app.listen(8080)
-
 console.log('Listening on port 8080')
